bd9e873927c23402a75d8a19e1e6b5656fbb1ba2
XuanNhi <letranxuannhi9a3@gmail.com>
2025-10-23 21:59:12 +0700

    Save current progress before merge

--- Assets/Script/CamDrag.cs
+++ Assets/Script/CamDrag.cs
@@ -12,22 +12,19 @@ public class CameraDrag : MonoBehaviour
         {
             lastMousePos = Input.mousePosition;
         }
-//bruhhhjakshdkj
+
         if (Input.GetMouseButton(0))
         {
             Vector3 delta = Input.mousePosition - lastMousePos;
 
-            // Lấy hướng ngang (right) và dọc (forward) theo camera
             Vector3 right = transform.right;
             Vector3 forward = transform.forward;
 
-            // Bỏ thành phần Y để camera chỉ di chuyển trong mặt phẳng XZ
             right.y = 0;
             forward.y = 0;
             right.Normalize();
             forward.Normalize();
 
-            // Tính vector di chuyển dựa trên delta chuột
             Vector3 move = (-right * delta.x - forward * delta.y) * dragSpeed;
 
             transform.position += move;
