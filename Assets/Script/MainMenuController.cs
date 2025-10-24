ea1e1fa44fe288b811d79cfa650902b81b36fbf6
XuanNhi <letranxuannhi9a3@gmail.com>
2025-10-24 16:05:25 +0700

    Update MainMenuController.cs

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 
 public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene1.0");
     }
 
     public void QuitGame()
@@ -14,3 +18,32 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 }
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+public static class PlayFromMainMenu
+{
+    static PlayFromMainMenu()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeChanged;
+    }
+
+    private static void OnPlayModeChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            string mainMenuScenePath = "Assets/Scenes/MainMenu.unity";
+
+
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene(mainMenuScenePath);
+            }
+            else
+            {
+                EditorApplication.isPlaying = false;
+            }
+        }
+    }
+}
+#endif
